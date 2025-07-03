b6784a8267644fe6c7127e2ff1e93b943a62d7b6
Bhavith <>
2025-07-03 21:37:05 +0530

    Changing the terminal ID

--- src/main/java/org/worldlineConsole/App.java
+++ src/main/java/org/worldlineConsole/App.java
@@ -8,7 +8,7 @@ public class App
     public static void main ( String[] args ) throws TimException
     {
         TerminalSettings settings = new TerminalSettings();
-        settings.setTerminalId("12345678");
+        settings.setTerminalId("25496444");
         settings.setAutoCommit(false);
         settings.setIntegratorId("0e6b1705-ab96-455b-9ba3-a77dd919d7a5");
         Terminal terminal = new com.six.timapi.Terminal(settings);
