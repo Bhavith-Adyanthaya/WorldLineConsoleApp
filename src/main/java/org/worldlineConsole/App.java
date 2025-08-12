5d43c29f478cacb4178814b617961af687dd1c3b
Bhavith <bhavith@Bhaviths-MacBook-Pro.local>
2025-08-12 11:17:48 +0530

    Fixing compile error

--- src/main/java/org/worldlineConsole/App.java
+++ src/main/java/org/worldlineConsole/App.java
@@ -12,7 +12,7 @@ public class App
         settings.setAutoCommit(false);
 
         settings.setIntegratorId("0e6b1705-ab96-455b-9ba3-a77dd919d7a5");
-        Terminal terminal = new com.six.timapi.Terminal(settings);\
+        Terminal terminal = new com.six.timapi.Terminal(settings);
 
         // temp comments
 
